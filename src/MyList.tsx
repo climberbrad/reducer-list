5d9832c6edcdabdc740b04b2792f274b6a4a00fc
Brad Jordan <climberbrad@users.noreply.github.com>
2022-10-07 16:44:18 -0600

    styling

--- src/MyList.tsx
+++ src/MyList.tsx
@@ -8,11 +8,11 @@ export const buttonCss = (state: PieState) => {
     : 'bg-sky-700 w-full py-1 border border-gray-100 rounded text-white hover:opacity-75 hover:bg-gray-400 hover:text-black font-semibold';
 };
 
-const cloudSVG = (
+const listSVG = (
   <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'
        className='w-6 h-6'>
     <path stroke-linecap='round' stroke-linejoin='round'
-          d='M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 002.25 15z' />
+          d='M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z' />
   </svg>
 );
 
@@ -37,10 +37,10 @@ const MyList = ({ saveList, selected, options }: Props): JSX.Element => {
   };
 
   const listPie = (pie: string): JSX.Element => (
-    <div className='flex justify-between py-1'>
-      <p className='text-gray-500 text-md pr-2 font-semibold whitespace-nowrap'>{pie} pie</p>
-      <input value='boo' type='checkbox' checked={state.pieList.includes(pie)} onClick={() =>
-        dispatch({ type: 'click', pie: pie })} className='h-6 w-4 rounded focus:outline-none' />
+    <div className='flex justify-between py-1' onClick={() =>
+      dispatch({ type: 'click', pie: pie })}>
+      <p className='text-gray-500 text-md pr-2 font-semibold whitespace-nowrap hover:opacity-50 hover:text-white'>{pie} pie</p>
+      <input value='boo' type='checkbox' checked={state.pieList.includes(pie)} className='h-6 w-4 rounded focus:outline-none' />
     </div>
   );
 
@@ -56,7 +56,7 @@ const MyList = ({ saveList, selected, options }: Props): JSX.Element => {
       <form onSubmit={handleSubmit}>
         <div className='container mx-auto bg-blue-300 rounded-xl shadow border pt-8 w-1/4'>
           <div className='flex justify-center'>
-            <span className='mt-0.5 text-blue-700'>{cloudSVG}</span>
+            <span className='mt-0.5 text-white'>{listSVG}</span>
             <span className='text-xl text-gray-700 font-semibold pl-2'>Reducer checklist</span>
           </div>
           <div className='p-2 mx-20 my-4'>
